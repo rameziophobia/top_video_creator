d4482d2ccd07b052462b2af55aa322c267a83fbb
Ramez <rameznoshy@gmail.com>
2020-05-23 20:21:11 +0200

    feat: link video_downloader with list_splitter

--- scripts/video_downloader.py
+++ scripts/video_downloader.py
@@ -1,10 +1,12 @@
 import requests
-
+import json
 test_url = "https://static-gamespotvideo.cbsistatic.com/vr/2016/12/03/Trailer_Persona5_ThePhantomThieves_20161203_4000.mp4"
 
 
 def downloadfile(name, url):
+    name = "".join(x for x in name if (x.isalnum() or x in [" "]))
     name = "../videos/" + name + ".mp4"
+    print(name)
     r = requests.get(url)
     print("****Connected****")
     f = open(name, 'wb')
@@ -16,5 +18,11 @@ def downloadfile(name, url):
     f.close()
 
 
-downloadfile("Persona5", test_url)
+with open("output.json") as json_file:
+    data = json.load(json_file)
+    for game in data[5: 8]:
+        if game['video_found']:
+            print(f"downloading {game['name']} {game['video_url']}")
+            downloadfile(game['name'], game['video_url'])
+            print(f"finished {game['name']}")
 
