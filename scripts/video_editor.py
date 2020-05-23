22f9e31a2b940abf9ad6d83432768d41ee9f49cf
mage1711 <ziadaha@hotmail.com>
2020-05-23 22:08:15 +0200

    added extra functionality to video editor

--- scripts/video_editor.py
+++ scripts/video_editor.py
@@ -1,13 +1,13 @@
 from moviepy.editor import *
-
+import json
 clips = []
-names = ["1", "2", "3"]
+names = []
 
 
 def addText(clip, name):
-    txt_clip = (TextClip(name, fontsize=50, color='white')
+    txt_clip = (TextClip(name, fontsize=80, color='white')
                 .set_position(('left', 'bottom'))
-                .set_duration(2))
+                .set_duration(8))
     clip = CompositeVideoClip([clip, txt_clip])
     return clip
 
@@ -15,25 +15,43 @@ def addText(clip, name):
 def addVideos(clips):
 
     i = 0
+
     for clip in clips:
         clip = clip.resize((1920, 1080))
         clips[i] = addText(clip, names[i])
 
         i = i+1
     final = concatenate_videoclips([clip for clip in clips])
+
     # final = concatenate_videoclips([clips[0], clips[1]])
 
     final.write_videofile("../videos/final.mp4", codec="libx264")
 
 
+def createVideo():
+    with open("output.json") as json_file:
+        data = json.load(json_file)
+        i = 0
+        for game in data[5:7]:
+            clips.append(VideoFileClip(f"../videos/{game['filename']}"))
+            clips[i] = clips[i].subclip(round(clips[i].duration * 0.35),
+                                        round(clips[i].duration * 0.35)+10)
+            i = i+1
+            names.append(game['name'])
+
+    addVideos(clips)
+
+
 if __name__ == "__main__":
-    clip1 = VideoFileClip("../videos/Persona5.mp4")
-    clip2 = VideoFileClip("../videos/BloodBorne.mp4")
-    clip3 = VideoFileClip("../videos/BloodBorne.mp4")
-    clip1 = clip1.subclip(60, 62)
-    clip2 = clip2.subclip(60, 62)
-    clip3 = clip3.subclip(40, 50)
-    clips.append(clip1)
-    clips.append(clip2)
-    clips.append(clip3)
+
+    with open("output.json") as json_file:
+        data = json.load(json_file)
+        i = 0
+        for game in data[9:12]:
+            clips.append(VideoFileClip(f"../videos/{game['filename']}"))
+            clips[i] = clips[i].subclip(round(clips[i].duration * 0.35),
+                                        round(clips[i].duration * 0.35)+10)
+            i = i+1
+            names.append(game['name'])
+
     addVideos(clips)
